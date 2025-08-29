6ed9c1e2aeb4b5497842e0b53800cbcb09d830ea
Nalu <pordosol699@gmail.com>
2025-08-29 10:47:54 -0300

    adicionei ; e coloquei uma explicação de como funciona um if e else simples de uma so linha

--- javascrip.js
+++ javascrip.js
@@ -1,25 +1,25 @@
-let nome = document.getElementById("nome")
-let numeroPokemon = document.getElementById("numeroPokemon")
-let img = document.getElementById("img")
-let tipo1 = document.getElementById("tipo1")
-let tipo2 = document.getElementById("tipo2")
-let altura = document.getElementById("altura")
-let peso = document.getElementById("peso")
-let ataque = document.getElementById("ataque")
-let defesa = document.getElementById("defesa")
-let velocidade = document.getElementById("velocidade")
-let ataqueEspecial = document.getElementById("ataqueEspecial")
-let icon = document.getElementById("icon")
-let propriedades = document.getElementById("propriedades")
-let tela = document.getElementById("tela")
-let audio = document.getElementById("audio")
-let erro = document.getElementById("erro")
+let nome = document.getElementById("nome");
+let numeroPokemon = document.getElementById("numeroPokemon");
+let img = document.getElementById("img");
+let tipo1 = document.getElementById("tipo1");
+let tipo2 = document.getElementById("tipo2");
+let altura = document.getElementById("altura");
+let peso = document.getElementById("peso");
+let ataque = document.getElementById("ataque");
+let defesa = document.getElementById("defesa");
+let velocidade = document.getElementById("velocidade");
+let ataqueEspecial = document.getElementById("ataqueEspecial");
+let icon = document.getElementById("icon");
+let propriedades = document.getElementById("propriedades");
+let tela = document.getElementById("tela");
+let audio = document.getElementById("audio");
+let erro = document.getElementById("erro");
 
-let ir = document.getElementById("ir")
-let voltar = document.getElementById("voltar")
+let ir = document.getElementById("ir");
+let voltar = document.getElementById("voltar");
 
-let pokemon = document.getElementById("pokemon")
-let numero = document.getElementById("numero")
+let pokemon = document.getElementById("pokemon");
+let numero = document.getElementById("numero");
 
 let numeroInicial = 0
 
@@ -44,23 +44,27 @@ function gerarPokemon() {
         .then((dados) => {
 
             numeroInicial = dados.id
-            tipo1.innerHTML = "Tipo: " + letraMaiuscula(dados.types[0].type.name)//como tipos é um vetor precisamos declarar qual indice queremos
-            tipo2.innerHTML = "Tipo 2: " + letraMaiuscula(dados.types[1] ? dados.types[1].type.name : "Não possui")
-            nome.innerHTML = (letraMaiuscula(dados.name))
-            numeroPokemon.innerHTML = "Nº: " + dados.id
-            img.src = dados.sprites.other.dream_world.front_default ? dados.sprites.other.dream_world.front_default : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + dados.id + ".png"
-            img.style.height = "141px"
-            img.style.width = "153px"
-            icon.src = dados.sprites.front_default ? dados.sprites.front_default : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/icons/" + dados.id + ".png"
-            altura.innerHTML = "Altura: 0," + dados.height + " Metros"
-            peso.innerHTML = "Peso: 0," + dados.weight + " Quilos"
-            ataque.innerHTML = "Ataque: " + dados.stats[1].base_stat
-            defesa.innerHTML = "Defesa: " + dados.stats[2].base_stat
-            velocidade.innerHTML = "Velocidade: " + dados.stats[5].base_stat
-            ataqueEspecial.innerHTML = "Especial: " + letraMaiuscula(dados.moves[0].move.name)
-            audio.src = dados.cries.latest
-            audio.play()
-            erro.innerHTML = ""
+            tipo1.innerHTML = "Tipo: " + letraMaiuscula(dados.types[0].type.name);//como tipos é um vetor precisamos declarar qual indice queremos
+            tipo2.innerHTML = "Tipo 2: " + letraMaiuscula(dados.types[1] ? dados.types[1].type.name : "Não possui");
+            //if simples se(?) existir indice 1 em dados.types ele exibe o nome do tipo senao(:) exibe nao possui
+
+            nome.innerHTML = letraMaiuscula(dados.name);
+            numeroPokemon.innerHTML = "Número: " + dados.id;
+            img.src = dados.sprites.other.dream_world.front_default ? dados.sprites.other.dream_world.front_default : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + dados.id + ".png";
+            //Se(?) existir dados.sprites.other.dream_world.front_default na API retorna essa imagem, senao(:) busca na API do github junto do id e carrega como png
+            
+            img.style.height = "141px"; //altura
+            img.style.width = "153px"; //largura
+            icon.src = dados.sprites.front_default ? dados.sprites.front_default : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/icons/" + dados.id + ".png";
+            altura.innerHTML = "Altura: 0," + dados.height + " Metros";
+            peso.innerHTML = "Peso: 0," + dados.weight + " Quilos";
+            ataque.innerHTML = "Ataque: " + dados.stats[1].base_stat;
+            defesa.innerHTML = "Defesa: " + dados.stats[2].base_stat;
+            velocidade.innerHTML = "Velocidade: " + dados.stats[5].base_stat;
+            ataqueEspecial.innerHTML = "Especial: " + letraMaiuscula(dados.moves[0].move.name);
+            audio.src = dados.cries.latest;
+            audio.play();
+            erro.innerHTML = "";
 
 
             if (propriedades.style.border = "none") {
@@ -122,7 +126,7 @@ function mudarPokemonFrente() {
                 tipo1.innerHTML = "Tipo: " + letraMaiuscula(dados.types[0].type.name)//como tipos é um vetor precisamos declarar qual indice queremos
                 tipo2.innerHTML = "Tipo 2: " + letraMaiuscula(dados.types[1] ? dados.types[1].type.name : "não possui")
                 nome.innerHTML = (letraMaiuscula(dados.name))
-                numeroPokemon.innerHTML = "Nº: " + dados.id
+                numeroPokemon.innerHTML = "Número: " + dados.id
                 img.src = dados.sprites.other.dream_world.front_default ? dados.sprites.other.dream_world.front_default : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + dados.id + ".png"
                 img.style.height = "141px"
                 img.style.width = "153px"
@@ -199,7 +203,7 @@ function mudarPokemonTras() {
                 tipo1.innerHTML = "Tipo: " + (letraMaiuscula(dados.types[0].type.name))//como tipos é um vetor precisamos declarar qual indice queremos
                 tipo2.innerHTML = "Tipo 2: " + letraMaiuscula(dados.types[1] ? dados.types[1].type.name : "não possui")
                 nome.innerHTML = (letraMaiuscula(dados.name))
-                numeroPokemon.innerHTML = "Nº: " + dados.id
+                numeroPokemon.innerHTML = "Número: " + dados.id
                 img.src = dados.sprites.other.dream_world.front_default ? dados.sprites.other.dream_world.front_default : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + dados.id + ".png"
                 img.style.height = "141px"
                 img.style.width = "153px"
